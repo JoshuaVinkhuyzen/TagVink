cc8fb4b0718e867ac85cb0895f0b9b766efbbf74
JoshuaVinkhuyzen <Joshua.vinkhuyzen@gmail.com>
2024-06-19 16:57:04 +0200

    Added the functionality for the item list and added it into the Application.py

--- Application.py
+++ Application.py
@@ -1,7 +1,7 @@
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QMainWindow, QComboBox, QHBoxLayout, QVBoxLayout, QWidget, QLabel, QSizePolicy, QFormLayout
 
-import TagWindow
+import TagWindow, ItemList
 
 
 class MainWindow(QMainWindow):
@@ -11,10 +11,16 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("TagVink")
         self.setMinimumSize(400, 300)
 
-        self.main_layout = QVBoxLayout(self)
+        self.main_layout = QHBoxLayout(self)
 
         tag_window = TagWindow.TagWindow()
         self.main_layout.addWidget(tag_window)
+
+        directory_path = ('D:/Programing/PycharmProjects/Kitepower_Tether_Inspection/Tether_Inspection_Software/'
+                          'Saved_Images/Parallel_1/1_Side')
+
+        item_list = ItemList.ItemList(directory_path)
+        self.main_layout.addWidget(item_list)
 
         central_widget = QWidget()
         central_widget.setLayout(self.main_layout)
